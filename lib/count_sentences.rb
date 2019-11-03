a728c53d803f918e16cecdc903937c8403843dd1
Michael Waldo <mbwcogic@yahoo.com>
2019-11-03 15:51:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
   end
 
   def count_sentences
-     self.split(/\.|\?|\!/).delete_if {|w| w.size < 1}.size
+     self.split(/\.|\?|\!/).delete_if {|w| w.size < 4}.size
      
      binding.pry
   end
